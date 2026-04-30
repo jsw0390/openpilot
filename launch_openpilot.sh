3106a4003d5057ce318e6bec4c06d4d826c5c94d
jungsunwoo1 <gobiztrip@gmail.com>
2026-04-30 22:18:14 +0900

    Revert "Update API_HOST and ATHENA_HOST to konik.ai"
    This reverts commit 52cd185669fe13e967bcab91db9f4c9ecef663c6.

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [[ "$(cat /data/params/d/EnableConnect)" == "2" ]]; then
-  export API_HOST="https://api.konik.ai/"
-  export ATHENA_HOST="wss://athena.konik.ai/"
+  export API_HOST="https://api.carrotpilot.app"
+  export ATHENA_HOST="wss://athena.carrotpilot.app"
 fi
 exec ./launch_chffrplus.sh
